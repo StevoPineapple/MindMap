c2a99dcc655333ff9e94eeea7d174ca9f6b17d53
Kjwwww <yunqing@sina.cn>
2018-05-31 14:02:12 +0800

    add notes

--- src/mindmap/nodes/MMEllipse.java
+++ src/mindmap/nodes/MMEllipse.java
@@ -11,18 +11,17 @@ import java.util.ArrayList;
 
 public class MMEllipse extends Node{
     private Text text;
-    private Ellipse ellipse;
+    private Ellipse ellipse; //新建椭圆形
     private Color fillColor = Color.WHITE;
-    private Color stroke= Color.web("#00BCD4");
+    private Color stroke= Color.web("#00BCD4"); // 鼠标没碰到之前图形的颜色
     private EdgeGroup edgeGroup;
-
 
     public MMEllipse(String text){
         this.text=new Text(text);
 
         ellipse = new Ellipse();
         ellipse.setFill(this.fillColor);
-        ellipse.setStroke(this.stroke);
+        ellipse.setStroke(this.stroke);   // 设置椭圆形的长半径和短半径
         ellipse.setRadiusX(this.text.getBoundsInLocal().getWidth() + 5);
         ellipse.setRadiusY(25);
         ellipse.setId("ellipse-"+nodes.size());
@@ -38,13 +37,13 @@ public class MMEllipse extends Node{
         registerDrag();
     }
 
-    private void registerDrag(){
+    private void registerDrag(){  // 拖动椭圆形
         MMEllipse self=this;
         //double delta = this.getS;
 
         this.setOnMouseDragged(new EventHandler<MouseEvent>() {
             @Override
-            public void handle(MouseEvent event) {
+            public void handle(MouseEvent event) { // 移动椭圆形
                 self.setTranslateX(event.getX() + self.getTranslateX() - ellipse.getRadiusX() );
                 self.setTranslateY(event.getY() + self.getTranslateY() - ellipse.getRadiusY() );
 

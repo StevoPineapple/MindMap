a88159c91bfa039086f503a3a8149c4f4a63daae
Steven Yang <new.logintesting@gmail.com>
2018-06-01 11:07:44 +0800

    Cleaned up

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.Group;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+        stage.setTitle("Hello World");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
+
     }
 
 
